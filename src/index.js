9b0516a324f4720b4c58ae437d6e1edc1da1ba57
Maryna Khmarska <111706884+myhappyday@users.noreply.github.com>
2023-08-08 15:58:40 +0300

    Adds basename in BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { App } from 'components/App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { App } from "components/App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter
-    // basename="/react-78-module-5"
-    >
+    <BrowserRouter basename="/react-78-module-5">
       <App />
     </BrowserRouter>
   </React.StrictMode>
