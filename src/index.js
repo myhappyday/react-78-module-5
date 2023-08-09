0c5e9ac364bbf55789d7c0ff84fc03f3053b2758
Maryna Khmarska <111706884+myhappyday@users.noreply.github.com>
2023-08-09 14:12:18 +0000

    Module 5 Lesson 10

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { App } from "components/App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { App } from 'components/App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/react-78-module-5">
+    <BrowserRouter
+    // basename="/react-78-module-5"
+    >
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- src/pages/Dogs.js
+++ src/pages/Dogs.js
@@ -1,20 +1,56 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useSearchParams } from 'react-router-dom';
 
 const Dogs = () => {
+  const [dogs, setDogs] = useState([
+    'dog-1',
+    'dog-2',
+    'dog-3',
+    'dog-4',
+    'dog-5',
+  ]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const dogId = searchParams.get('dogId') ?? '';
+
+  console.log('setDogs:', setDogs);
+  
   // useEffect(() => {
   // HTTP запит, якщо потрібно
   // }, [])
 
+  const updateQueryString = evt => {
+    const dogIdValue = evt.target.value;
+    if (dogIdValue === '') {
+      return setSearchParams({});
+    }
+    setSearchParams({ dogId: dogIdValue });
+  };
+
+  const visibleDogs = dogs.filter(dog => dog.includes(dogId));
+
   return (
     <div>
+      <input type="text" value={dogId} onChange={updateQueryString} />
+      {/* <button
+        onClick={() => {
+          setSearchParams({ c: 'hello' });
+        }}
+      >
+        Change searchParams
+      </button> */}
       {/* Collection of dogs */}
-      {['dog-1', 'dog-2', 'dog-3', 'dog-4', 'dog-5'].map(dog => {
-        return (
-          <Link key={dog} to={`${dog}`}>
-            {dog}
-          </Link>
-        );
-      })}
+      <ul>
+        {
+          // ['dog-1', 'dog-2', 'dog-3', 'dog-4', 'dog-5']
+          visibleDogs.map(dog => {
+            return (
+              <li key={dog}>
+                <Link to={`${dog}`}>{dog}</Link>
+              </li>
+            );
+          })
+        }
+      </ul>
     </div>
   );
 };
